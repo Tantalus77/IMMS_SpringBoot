61340ffae9c5a299d0632c833833b367e0d4c44f
普朗千克 <1094628341@qq.com>
2023-03-29 00:36:45 +0800

    1.两个测试用例

--- src/test/java/imms/ImmsSpringBootApplicationTests.java
+++ src/test/java/imms/ImmsSpringBootApplicationTests.java
@@ -1,13 +1,39 @@
 package imms;
 
+import imms.model.Room;
+import imms.model.User;
+import imms.service.RoomServiceInterface;
+import imms.service.UserServiceInterface;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class ImmsSpringBootApplicationTests {
 
+    @Autowired
+    private UserServiceInterface userServiceInterface;
+
+    @Autowired
+    private RoomServiceInterface roomServiceInterface;
     @Test
-    void contextLoads() {
+    void testUserSelect() {
+        User user = new User();
+        user.setUserName("张");
+
+        List<User> select = userServiceInterface.select(user);
+        System.out.println(select);
+    }
+
+    @Test
+    void testRoomSelect(){
+        Room room = new Room();
+        room.setRoomAddress("沙河");
+
+        List<Room> select = roomServiceInterface.selectRoom(room);
+        System.out.println(select);
     }
 
 }
